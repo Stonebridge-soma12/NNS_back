b37502239dc592cba4648343d79e4a7e3d90db99
Doin Cha <stainblue1129@gmail.com>
2021-07-13 06:01:00 +0900

    feat: error response util function

---
+++ service/response.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// write response helper
+type responseBody map[string]interface{}
+
+func writeJson(w http.ResponseWriter, code int, data interface{}) error {
+	jsoned, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_, err = w.Write(jsoned)
+	return err
+}
+
+// write error response helper
+type ErrCode string
+
+const (
+	// 400
+	ErrInvalidPathParm    ErrCode = "Invalid Path Parameter"
+	ErrInvalidQueryParm   ErrCode = "Invalid Query Parameter"
+	ErrInvalidRequestBody ErrCode = "Invalid Request Body"
+	ErrBadRequest         ErrCode = "Bad Request"
+	ErrNotFound           ErrCode = "Not Found"
+
+	// 500
+	ErrInternalServerError ErrCode = "Internal Server Error"
+)
+
+type ErrBody struct {
+	StatusCode int     `json:"statusCode"` // http status code
+	ErrMsg     ErrCode `json:"errMsg"`
+}
+
+func writeError(w http.ResponseWriter, code int, errCode ErrCode) error {
+	body := ErrBody{
+		StatusCode: code,
+		ErrMsg:     errCode,
+	}
+	return writeJson(w, code, body)
+}
