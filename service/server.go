ce1cd19aec57f1a0b4d08020899116a807c5be31
Doin Cha <stainblue1129@gmail.com>
2021-07-21 14:53:57 +0900

    fix: cors

--- service/server.go
+++ service/server.go
@@ -21,23 +21,23 @@ func (e Env) Start(port string) {
 
 	router.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
-	}).Methods(http.MethodGet)
+	}).Methods(http.MethodGet, http.MethodOptions)
 
 	// project
-	router.HandleFunc("/api/projects", e.GetProjectListHandler).Methods(http.MethodGet)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}", e.GetProjectHandler).Methods(http.MethodGet)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}/content", e.GetProjectContentHandler).Methods(http.MethodGet)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}/config", e.GetProjectConfigHandler).Methods(http.MethodGet)
-	router.HandleFunc("/api/project", e.CreateProjectHandler).Methods(http.MethodPost)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}/info", e.UpdateProjectInfoHandler).Methods(http.MethodPut)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}/content", e.UpdateProjectContentHandler).Methods(http.MethodPut)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}/config", e.UpdateProjectConfigHandler).Methods(http.MethodPut)
-	router.HandleFunc("/api/project/{projectNo:[0-9]+}", e.DeleteProjectHandler).Methods(http.MethodDelete)
+	router.HandleFunc("/api/projects", e.GetProjectListHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}", e.GetProjectHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}/content", e.GetProjectContentHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}/config", e.GetProjectConfigHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/api/project", e.CreateProjectHandler).Methods(http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}/info", e.UpdateProjectInfoHandler).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}/content", e.UpdateProjectContentHandler).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}/config", e.UpdateProjectConfigHandler).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/api/project/{projectNo:[0-9]+}", e.DeleteProjectHandler).Methods(http.MethodDelete, http.MethodOptions)
 	
 	router.Use(handlers.CORS(
-		handlers.AllowedMethods([]string{"*"}),
+		handlers.AllowedMethods([]string{http.MethodOptions, http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete}),
 		handlers.AllowedOrigins([]string{"*"}),
-		handlers.AllowedHeaders([]string{"*"}),
+		handlers.AllowedHeaders([]string{"Accept", "Accept-Language", "Content-Type", "Content-Language", "Origin"}),
 	))
 
 	srv := &http.Server{
