2ab9f17cfb2e9d1df06ee3b5ca1842fcdaf2dab0
Doin Cha <stainblue1129@gmail.com>
2021-08-18 22:11:52 +0900

    chore: remove unnecessary comments

--- ws/client.go
+++ ws/client.go
@@ -8,61 +8,6 @@ import (
 	"time"
 )
 
-//
-//import (
-//	"github.com/gorilla/websocket"
-//)
-//
-//type Client struct {
-//	id       int
-//	hub      *Hub
-//	socket   *websocket.Conn
-//	outbound chan []byte
-//}
-//
-//func newClient(hub *Hub, socket *websocket.Conn) *Client {
-//	return &Client{
-//		hub:      hub,
-//		socket:   socket,
-//		outbound: make(chan []byte),
-//	}
-//}
-//
-//func (c *Client) read() {
-//	defer func() {
-//		c.hub.unregister <- c
-//	}()
-//
-//	for {
-//		_, data, err := c.socket.ReadMessage()
-//		if err != nil {
-//			break
-//		}
-//
-//		c.hub.onMessage(data, c)
-//	}
-//}
-//
-//func (c *Client) write() {
-//	for {
-//		select {
-//		case data, ok := <-c.outbound:
-//			if !ok {
-//				c.socket.WriteMessage(websocket.CloseMessage, []byte{})
-//				return
-//			}
-//
-//			c.socket.WriteMessage(websocket.TextMessage, data)
-//		}
-//	}
-//}
-//
-//func (c Client) close() {
-//	c.socket.Close()
-//	close(c.outbound)
-//}
-
-
 const (
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
