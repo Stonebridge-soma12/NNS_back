b3328c263b2f2c24f79c7d267884cb5b6bbe1998
Doin Cha <stainblue1129@gmail.com>
2021-08-20 00:00:58 +0900

    chore: remove unuse files

--- ws/message/message.go
+++
@@ -1,9 +0,0 @@
-package message
-
-type Message struct {
-	Type    MessageType `json:"type"`
-	UserID  int         `json:"userId"`
-	Content string      `json:"content"`
-	X       int         `json:"x"`
-	Y       int         `json:"y"`
-}
