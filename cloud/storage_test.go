9a44cdd7cde669278dfe91b355feeb87afe45329
Doin Cha <stainblue1129@gmail.com>
2021-09-07 16:53:55 +0900

    NNS-110 test: use assert

--- cloud/storage_test.go
+++ cloud/storage_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/stretchr/testify/assert"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,6 +15,8 @@ import (
 )
 
 func TestAwsS3Client_Put(t *testing.T) {
+	assertions := assert.New(t)
+
 	// Generate test client
 	awsAccessKeyId := os.Getenv("AWS_ACCESS_KEY_ID")
 	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
@@ -24,10 +27,7 @@ func TestAwsS3Client_Put(t *testing.T) {
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(awsAccessKeyId, awsSecretAccessKey, awsSessionToken)),
 		config.WithRegion("ap-northeast-2"),
 	)
-
-	if err != nil {
-		t.Errorf("failed to load aws config: %+v", err)
-	}
+	assertions.Nil(err)
 
 	s3Client := s3.NewFromConfig(cfg)
 
@@ -36,43 +36,30 @@ func TestAwsS3Client_Put(t *testing.T) {
 		BucketName: imageBucketName,
 	}
 
-	file, err := os.Open("TestImage.jpg")
-	if err != nil {
-		t.Errorf("failed to open file: %+v", err)
-	}
+	const testFileName = "TestImage.jpg"
+	assertions.FileExists(testFileName)
+	file, err := os.Open(testFileName)
+	assertions.Nil(err)
 
 	objectUrl, err := awsS3Client.Put(file)
-	if err != nil {
-		t.Errorf("failed to put object: %+v", err)
-	}
+	assertions.Nil(err)
 
 	t.Logf("object url : %s", objectUrl)
 	resp, err := http.Get(objectUrl)
-	if err != nil {
-		t.Errorf("failed to get image from object url: %+v", err)
-	}
+	assertions.Nil(err)
 	defer resp.Body.Close()
 
 	// compare
-	if _, err := file.Seek(0, io.SeekStart); err != nil {
-		t.Errorf("failed to file seek: %+v", err)
-	}
+	_, err = file.Seek(0, io.SeekStart)
+	assertions.Nil(err)
 
 	baseFileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		t.Errorf("failed to read base file: %+v", err)
-	}
+	assertions.Nil(err)
 
 	responseFileBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Errorf("failed to read response file: %+v", err)
-	}
+	assertions.Nil(err)
 
 	baseChecksum := sha256.Sum256(baseFileBytes)
 	responseChecksum := sha256.Sum256(responseFileBytes)
-	if baseChecksum != responseChecksum {
-		t.Logf("base checksum : %x", baseChecksum)
-		t.Logf("response checksum : %x", responseChecksum)
-		t.Fatal("image not equal")
-	}
+	assertions.Equal(baseChecksum, responseChecksum)
 }
--- go.mod
+++ go.mod
@@ -25,6 +25,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tidwall/gjson v1.8.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
