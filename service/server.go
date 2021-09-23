cc7f85050877024a3baae5797ea8844df19666e2
Doin Cha <stainblue1129@gmail.com>
2021-09-24 04:38:35 +0900

    [NNS-110] fix: change handler(UpdateFileConfig) method (POST -> PUT)

--- service/server.go
+++ service/server.go
@@ -116,7 +116,7 @@ func Start(port string, logger *zap.SugaredLogger, db *sqlx.DB, sessionStore ses
 
 	authRouter.HandleFunc("/api/datasets", datasetHandler.GetList).Methods(_Get...)
 	authRouter.HandleFunc("/api/dataset/file", datasetHandler.UploadFile).Methods(_Post...)
-	authRouter.HandleFunc("/api/dataset", datasetHandler.UpdateFileConfig).Methods(_Post...)
+	authRouter.HandleFunc("/api/dataset", datasetHandler.UpdateFileConfig).Methods(_Put...)
 	authRouter.HandleFunc("/api/dataset/{datasetId:[0-9]+}", datasetHandler.DeleteDataset).Methods(_Delete...)
 
 	authRouter.HandleFunc("/api/dataset/library", datasetHandler.GetLibraryList).Methods(_Get...)
