3be7972ab4c51f7d5adbce9e33e28ebad10e58fd
elixter <ltw971@naver.com>
2021-10-06 14:24:10 +0900

    [NNS-145] chore : Applied query builder on epoch delete method

--- train/epochRepository.go
+++ train/epochRepository.go
@@ -5,6 +5,6 @@ import "nns_back/query"
 type EpochRepository interface {
 	Insert(epoch Epoch) error
 	Find(opts ...query.Option) (Epoch, error)
-	Delete(opts ...Option) error
+	Delete(opts ...query.Option) error
 	FindAll(opts ...query.Option) ([]Epoch, error)
 }
